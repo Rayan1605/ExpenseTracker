aeee78cef86533df06d5b5b755314e1f5767bbe5
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-09 16:08:24 +0100

    AddingCss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import ExpenseItem  from "./Components/ExpenseItem";
+//Remember the simple rule is anythat that start with a lower case letter is a
+// built in  html component when it uppercase it is a custom so alwasy start with uppercase
 function App() {
   return (
    <div>
         <h1>Lets' get Started</h1>
-       <p>This is also visible</p>
+         <ExpenseItem></ExpenseItem>
    </div>
   );
 }
