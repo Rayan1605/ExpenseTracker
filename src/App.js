acbe63ad4647325edcea61f1e7160cdbd8e69547
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-12 15:44:18 +0100

    AddingCss

--- src/App.js
+++ src/App.js
@@ -2,10 +2,42 @@ import ExpenseItem  from "./Components/ExpenseItem";
 //Remember the simple rule is anythat that start with a lower case letter is a
 // built in  html component when it uppercase it is a custom so alwasy start with uppercase
 function App() {
-  return (
+    const expenses = [ //so here we are creating an array of objects
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+  return ( //so here you can see naming the title in expenses object as title so when we use it again we need to use the same name
    <div>
         <h1>Lets' get Started</h1>
-         <ExpenseItem></ExpenseItem>
+         <ExpenseItem title = {expenses[0].title}
+                      amount = {expenses[0].title}
+                      date = {expenses[0].date}></ExpenseItem>
+       <ExpenseItem title = {expenses[1].title}
+                           amount = {expenses[1].title}
+                           date = {expenses[1].date}></ExpenseItem>
+       <ExpenseItem  title = {expenses[2].title}
+                     amount = {expenses[2].title}
+                     date = {expenses[2].date}></ExpenseItem>
+       <ExpenseItem  title = {expenses[3].title}
+                     amount = {expenses[3].title}
+                     date = {expenses[3].date}></ExpenseItem>
    </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css'; // import the css file to be used in this component
 //then import the class name using className in the div tag and add the
 //class name to the css file and add the style you want to the class name
-function ExpenseItem (){
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
+function ExpenseItem (prop) // React will ensure that we get one parameter for each component
+//it will be an object and it will contain all the data that we pass to the component and you can name
+//whatever you want -> so you say prop then whatever you name your thing you put the same and it will give the data
+{  // so we named our thing amount -> so we acess the prop object and then the amount property and it will give us the data
     return (
         <div className={"expense-item"}>
-            <div>{expenseDate.toISOString()}</div>
+            <div>{prop.date.toISOString()}</div>
     <div className={"expense-item__description"}>
-        <h2>{expenseTitle}</h2>
-        <div className={"expense-item__price"}>{expenseAmount}</div>
+        <h2>{prop.title}</h2>
+        <div className={"expense-item__price"}>{prop.amount}</div>
     </div>
 
     </div>
