4e1dfc240d7518e0cc7f4464b30c14e134cbfa32
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-17 19:10:42 +0100

    AddingMore

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -17,7 +17,9 @@ const ExpenseForm = () => {
                 <input type="date" min="2020-01-01" step="2025-12-31"/>
             </div>
         </div>
-
+<div className={"new-expense__actions"}>
+<button type = "submit">Add Expense</button>
+</div>
 
     </form>
 }
