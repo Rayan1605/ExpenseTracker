dc4a4497e07e85b647b86821972998830c395236
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-17 19:06:49 +0100

    AddingMore

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./newExpense.css";
 
 const ExpenseForm = () => {
-    return <form></form>
+    return <form>
+        <div className="new-expense__controls">
+            <div className="new-expense__control"></div>
+            <label>Title</label>
+            <input type="text" />
+        </div>
+
+
+    </form>
 }
 
 export default ExpenseForm;
