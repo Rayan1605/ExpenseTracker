4408100d83e7e3257b09ea46445752af687fedb8
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-17 18:25:11 +0100

    AddingMore

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -3,19 +3,17 @@ import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    function clickHandler() {
-        console.log('Clicked!!!');
-    }
 
-    //To add an event listener to a component, you need to add the event listener to the
-    // component like you would in html with the onClick attribute. Then write the function
-    //YOU can also use an anonymous function like this onClick={() => {console.log('Clicked!!!');}}
+    let title = props.title;
+    function clickHandler() {
+       title = 'Updated';
+    }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick= {clickHandler} > Change Title</button>
