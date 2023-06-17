2de69f00513c31874ad7609025f4f2968be53729
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-17 19:11:04 +0100

    AddingMore

--- src/Components/NewExpense/newExpense.js
+++ src/Components/NewExpense/newExpense.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './newExpense.css';
+import ExpenseForm from './ExpenseForm';
 import expenses from "../Expenses/Expenses";
 const NewExpense = () => {
 return <div className="new-expenses">
