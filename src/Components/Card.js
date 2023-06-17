b89b8f6f094bc7812affc5282ac5ed8622ff27a2
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-17 18:09:33 +0100

    AddingMore

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,8 @@
 import './Card.css';
 
 function Card(props) {
-    const classes = 'card ' + props.className;
+    const classes = 'card ' + props.className; // this is soo other css classes can be added to the
+    // card component
 
     return <div className={classes}>{props.children}</div>;
 }
