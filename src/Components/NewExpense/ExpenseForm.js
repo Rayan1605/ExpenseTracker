99e1e01f0b2ef4fca40825cbaa4ba4b7dcaf8785
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-18 19:44:00 +0100

    AddingMore

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -3,12 +3,22 @@ import React, {useState} from "react";
 import "./newExpenses.css";
 
 
+
 const ExpenseForm = () => {
     const [enteredTitle,setEnteredTitle] = useState('');
+    const [enteredAmount,setEnteredAmount] = useState('');
+    const [enteredDate,setEnteredDate] = useState('');
     const TitleChangeHandler = (event) => {
 
         setEnteredTitle(event.target.value); // for saving user information
 
+    }
+
+    function AmountChange(event) {
+        setEnteredAmount(event.target.value); // for saving user information
+    }
+    function DateChanged(event) {
+        setEnteredDate(event.target.value); // for saving user information
     }
     return <form>
         <div className="new-expense__controls">
@@ -18,11 +28,11 @@ const ExpenseForm = () => {
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01"/>
+                <input type="number" min="0.01" step="0.01" onChange={AmountChange}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2020-01-01" step="2025-12-31"/>
+                <input type="date" min="2020-01-01" step="2025-12-31" onChange={DateChanged}/>
             </div>
         </div>
 <div className="new-expense__actions">
